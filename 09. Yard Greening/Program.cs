0e11a99e8371228daae523189496a19264b6e09e
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-04-22 04:21:40 +0300

    Add files via upload

---
+++ 09. Yard Greening/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace YardGreening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double area = double.Parse(Console.ReadLine());
+            double sum = area * 7.61;
+            double discount = (sum * 18) / 100;
+            double totalSum = sum - discount;
+            Console.WriteLine($"The final price is: {totalSum} lv.");
+            Console.WriteLine($"The discount is: {discount} lv.");
+        }
+
+    }
+
+}
