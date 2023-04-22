275279dfbcc6fc1fbbad0cbfb73593e7f71d8304
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-04-22 04:20:29 +0300

    Add files via upload

---
+++ 08. Pet Shop/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tinsDogs = int.Parse(Console.ReadLine());
+            int tinsCats = int.Parse(Console.ReadLine());
+            double sum = (tinsDogs * 2.50) + (tinsCats * 4.00);
+            Console.WriteLine($"{sum} lv.");
+        }
+    }
+}
